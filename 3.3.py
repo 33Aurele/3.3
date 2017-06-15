05c7c2f1c222bada837a04ee066a5cc9eb0f4a38
33Aurele <3.3.aurele@gmail.com>
2017-06-16 00:15:46 +0900

    fixed bugs. addd links. changed css

--- 3.3.py
+++ 3.3.py
@@ -3,24 +3,23 @@ from PyOrgMode.PyOrgMode import PyOrgMode
 
 base = PyOrgMode.OrgDataStructure()
 
-base.load_from_file("3.3.org")
+base.load_from_file("temp.org")
 
 for h1 in base.root.content:
     for h2 in h1.content:
-        try :
-            for h3 in h2.content:
-                try :
-                    print ("<div class=\"block ",end='')
-                    print (' '.join((h3.tags+h2.tags)+h1.tags),end='')
-                    print ("\">")
-                    
-                    print ("<p><h1>",end='')
-                    print (h3.heading.rstrip(),end='')
-                    print ("</h1>")
-                    
-                    print (''.join(h3.content),end='')
-                    print ("</p></div>")
-                except:
-                    pass
-        except:
-            pass
+        for h3 in h2.content:
+            print ("<div class=\"block ",end='')
+            print (' '.join((h3.tags+h2.tags)+h1.tags),end='')
+            print ("\">")
+            
+            print ("<p><h2>",end='')
+            print (h3.heading.rstrip(),end='')
+            print ("</h2>")
+            
+            print("<h5> Tags: ",end='')
+            print(', '.join((h1.tags+h2.tags)+h3.tags),end='')
+            print("</h5>",end='')
+            
+            print ('<br>'.join(h3.content),end='')
+            print ("</p></div>")
+                
--- 3.3.sh
+++ 3.3.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
+# replace todos with something more readable
+cp 3.3.org temp.org
+sed -i 's/TODO/\(needs to be tested\)/g' temp.org
+
+#removing empty lines
+sed -i '/^$/d' temp.org
+
 python3 3.3.py > body.html
 
 cat html/head.html body.html html/foot.html > html/3.3.html
 
+#dirty trick to replace org-links with html links
+sed -i 's/\[\[/\<a href\=\"/g' html/3.3.html
+sed -i 's/\]\[/\"\>/g' html/3.3.html
+sed -i 's/\]\]/\<\/a\>/g' html/3.3.html
+
 rm body.html
+rm temp.org
