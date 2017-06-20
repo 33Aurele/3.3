be376c0bb968d6a3eaf0bee289e16fd298fd60cf
33Aurele <3.3.aurele@gmail.com>
2017-06-20 23:27:35 +0900

    clean tag printing

--- 3.3.py
+++ 3.3.py
@@ -8,6 +8,15 @@ base.load_from_file("temp.org")
 for h1 in base.root.content:
     for h2 in h1.content:
         for h3 in h2.content:
+            # clean tags for printing
+            t1 = h1.tags
+            if ("@all" in t1): 
+                t1 = ["@all"]
+            t2 = h2.tags
+            if ("all" in t2):
+                t2 = ["all"]
+            t3 = h3.tags
+
             print ("<div class=\"block ",end='')
             print (' '.join((h3.tags+h2.tags)+h1.tags),end='')
             print ("\">")
@@ -17,7 +26,7 @@ for h1 in base.root.content:
             print ("</h2>")
             
             print("<h5> Tags: ",end='')
-            print(', '.join((h1.tags+h2.tags)+h3.tags),end='')
+            print(', '.join((t1+t2)+t3),end='')
             print("</h5>",end='')
             
             print ('<br>'.join(h3.content),end='')
