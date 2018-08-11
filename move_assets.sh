41177ec475a9a14b29764c92aeace53a2345faec
33Aurele <3.3.aurele@gmail.com>
2018-08-11 16:28:01 +0200

    updating script for images

--- move_assets.sh
+++ move_assets.sh
@@ -4,6 +4,9 @@ cp html/3.3.html apk/Third\ Strike\ Tech/src/main/assets
 cp html/contribute.html apk/Third\ Strike\ Tech/src/main/assets
 cp html/links.html apk/Third\ Strike\ Tech/src/main/assets
 cp html/style/style.css apk/Third\ Strike\ Tech/src/main/assets/style
+cp html/img/sa1.png apk/Third\ Strike\ Tech/src/main/assets/img/sa1.png
+cp html/img/sa2.png apk/Third\ Strike\ Tech/src/main/assets/img/sa2.png
+cp html/img/sa3.png apk/Third\ Strike\ Tech/src/main/assets/img/sa3.png
 
 cd apk/Third\ Strike\ Tech/src/main/assets
 
