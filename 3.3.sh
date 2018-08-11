2aa8bf6e1f6db4ba1206e89d0d5d7822b970f816
33Aurele <3.3.aurele@gmail.com>
2018-08-11 15:47:44 +0200

    more notes for makoto and ryu

--- 3.3.sh
+++ 3.3.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # join every .org file
-
 cd org
 cat makoto.org akuma.org alex.org chunli.org dudley.org elena.org gill.org hugo.org ibuki.org ken.org necro.org oro.org q.org remy.org ryu.org sean.org twelve.org urien.org yang.org yun.org other.org all.org > ../3.3.org
 cd ..
@@ -19,14 +18,14 @@ sed -i 's/(SA1)/\<img src\=\"img\/sa1.png\" \/\>/g' temp.org
 sed -i 's/(SA2)/\<img src\=\"img\/sa2.png\" \/\>/g' temp.org
 sed -i 's/(SA3)/\<img src\=\"img\/sa3.png\" \/\>/g' temp.org
 
-#removing empty lines
+# removing empty lines
 sed -i '/^$/d' temp.org
 
 python3 3.3.py > body.html
 
 cat html/head.html body.html html/foot.html > html/3.3.html
 
-#dirty trick to replace org-links with html links
+# dirty trick to replace org-links with html links
 sed -i 's/\[\[/\<a href\=\"/g' html/3.3.html
 sed -i 's/\]\[/\"\>/g' html/3.3.html
 sed -i 's/\]\]/\<\/a\>/g' html/3.3.html
